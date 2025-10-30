0d2b52e65447c6e9e34ae1e831aae755df9ef49c
Jota <jotapaulo.box@gmail.com>
2025-10-30 15:56:17 -0300

    2 commit camera e passagem
    adiciona ajuste simples na câmera , para cobrir a visão do campo inteiro , adiciona também uma verificação de dados em cada célula , para saber se a mesma é possível passar ou não

--- Roguelike/Assets/Scripts/BoardManager.cs
+++ Roguelike/Assets/Scripts/BoardManager.cs
@@ -1,9 +1,19 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+
+
+
 public class BoardManager : MonoBehaviour
 {
-    private Tilemap m_tilemap;
+
+    public class CellData
+    {
+        public bool Passable;
+    }
+
+    private CellData[,] m_BoardData;
+    private Tilemap m_Tilemap;
 
     public int width;
     public int height;
@@ -14,24 +24,28 @@ public class BoardManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        m_tilemap = GetComponentInChildren<Tilemap>();
+        m_Tilemap = GetComponentInChildren<Tilemap>();
+        m_BoardData = new CellData[width, height];
 
         for (int y = 0; y < height; ++y)
         {
             for (int x = 0; x < width; ++x)
             {
                 Tile tile;
+                m_BoardData[x, y] = new CellData();
 
                 if (x == 0 || y == 0 || x == width - 1 || y == height - 1)
                 {
                     tile = WallTiles[Random.Range(0, WallTiles.Length)];
+                    m_BoardData[x, y].Passable = false;
                 }
                 else
                 {
                     tile = GroundTiles[Random.Range(0, GroundTiles.Length)];
+                    m_BoardData[x, y].Passable = true;
                 }
 
-                m_tilemap.SetTile(new Vector3Int(x, y, 0), tile);
+                m_Tilemap.SetTile(new Vector3Int(x, y, 0), tile);
             }
         }
 
@@ -43,3 +57,5 @@ public class BoardManager : MonoBehaviour
         
     }
 }
+
+
